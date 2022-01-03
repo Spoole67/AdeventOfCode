c813f469df709e958183b601c321b888a39c4709
Niklas <niklask456gmail.com>
2022-01-03 20:21:12 +0100

    lägg till

--- Day2/Day2Dive_again.py
+++ Day2/Day2Dive_again.py
@@ -1,16 +1,41 @@
 
 # Vad är det för kod!
 
-class Submarie:
+class Submarine:
 
-    def __init__(self, data):
-        self.depth = None
-        self.hori = None
+    def __init__(self, depth, hori):
+        self.depth = depth
+        self.hori = hori
 
     def __str__(self):
         return print(f"Djupet är {self.depth} och längden är {self.hori}.")
 
-    def multiplicate(self):
+    def mult(self):
         return self.depth * self.hori
+
+    # down, forward or up
+    # increase the values of these to the object
+    def add(self, direction, value):
+        if direction == "forward":
+            self.hori += value
+        elif direction == "up":
+            self.depth -= value
+        elif direction == "down":
+            self.depth += value
+
+
+
+# Create object
+# Main
+sub = Submarine(0,0)
+with open("input_Day2.txt", "r", encoding = "utf-8") as file:
+    for row in file:
+        row_stripped = row.strip()
+        row_list = row_stripped.split(" ")
+        sub.add(row_list[0], int(row_list[1]))
+        #
+
+print(sub.mult())
+
 
 
