3237317d760f3037420b85cde2033ae16ec871d5
Niklas <niklask456gmail.com>
2022-01-04 10:02:55 +0100

    Lägg upp först fill för day 3

---
+++ Day3/Day3Binary.py
@@ -0,0 +1,11 @@
+# Read file
+# Split the str into different ints? Or can it be done with some kind of bit splitter?
+# Create a class which has five instance variables
+# create method which have count all the instance variables
+# 0 --> -= 1
+# If total <0 --> 0
+# 1 --> += 1
+# If total >0 --> 1
+# creata a reverese method! A method which reverse all the binary numbers
+# creat a binary to decimal method (prob exist )
+# create a mult method!
